c8d33d330d8275f27e07762e44a9aed071736a92
Znu <jinwoo3407@gmail.com>
2026-01-27 03:15:17 +0900

    feat: solve programmers lv1 - repucahse_user_product

---
+++ Programmers/Lv1/SELECT1/Lv1_repurchase_user_product.sql
@@ -0,0 +1,10 @@
+/*
+- Table: ONLINE_SALE
+- Goal: 재구매한 회원 ID와 재구매한 상품 ID를 출력 (회원 ID를 기준으로 오름차순, 회원 ID가 같다면 상품 ID를 기준으로 내림차순)
+- 핵심 컬럼: USER_ID , PRODUCT_ID
+*/
+SELECT USER_ID , PRODUCT_ID
+FROM ONLINE_SALE
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) > 1
+ORDER BY USER_ID ASC, PRODUCT_ID DESC;
