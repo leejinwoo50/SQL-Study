b9ae21c07a1866b8cdb07114d334c91e89ff00d1
Znu <jinwoo3407@gmail.com>
2026-01-28 03:19:29 +0900

    feat: solve programmers lv1 - check id,name of sick

---
+++ Programmers/Lv1/SELECT1/lv1_chk_id_sick.sql
@@ -0,0 +1,10 @@
+
+/*
+- Table: ANIMAL_INS
+- Goal: 모든 동물의 id와 이름 조회, 결과는 ANIMAL_ID 순
+- 핵심 컬럼: ANIMAL_ID, NAME
+*/
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION IN('Sick')
+ORDER BY ANIMAL_ID ASC
