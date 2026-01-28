032c2bd2ce2cddf8ef72c7d96e362b64a05248c9
Znu <jinwoo3407@gmail.com>
2026-01-29 00:48:05 +0900

    feat: solve programmers lv1 - find python developers

---
+++ Programmers/Lv1/SELECT1/lv1_find_python_dev.sql
@@ -0,0 +1,9 @@
+/*
+- Table: DEVELOPER_INFOS
+- Goal: 테이블에서 Python 스킬을 가진 개발자의 정보를 조회
+- 핵심 컬럼: ID, EMAIL,FIRST_NAME, LAST_NAME ,SKILL_1,2,3
+*/
+SELECT ID, EMAIL,FIRST_NAME, LAST_NAME
+FROM DEVELOPER_INFOS
+WHERE SKILL_1 ='Python' OR SKILL_2 ='Python' OR SKILL_3 ='Python'
+ORDER BY ID ASC
